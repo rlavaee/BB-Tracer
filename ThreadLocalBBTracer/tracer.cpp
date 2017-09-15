a0914149b96f234806064ee177e196ca97db18ed
Rahman Lavaee <rlavaee@icloud.com>
2017-09-15 16:21:52 -0400

    Update tracer.cpp

--- ThreadLocalBBTracer/tracer.cpp
+++ ThreadLocalBBTracer/tracer.cpp
@@ -1,5 +1,5 @@
 /** 
- * Copyright (c) 2017, Rahman Lavaee All rights oserved.
+ * Copyright (c) 2017, Rahman Lavaee All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
